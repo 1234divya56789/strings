b2ef24720721687a9f35112351adb28f7dc1d47a
1234divya56789 <133005614+1234divya56789@users.noreply.github.com>
2023-11-06 10:43:12 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,43 @@
+/*  JFM1T8_Assignment5:
+
+    Write a program to reverse a string using StringBuffer. Reverse entire sentence in string and also reverse each word in the string. 
+    Prompt the user input from the terminal.
+    
+    Sample input: Learning made easy at bitLabs
+    
+    Expected output:
+    reversing entire string: sbaLtib ta ysae edam gninraeL
+    reversing each word at its place: gninraeL edam ysae ta sbaLtib  
+*/
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+//main method
+
+//declare variables and take input from user
+
+//creat stringbuffer object for string reversing
+
+//reverse input string
+
+//print entire string result
+
+//call reverWordInMyString method
+
+/* 
+   Use split() method of String class splits
+   a string in several strings based on the
+   delimiter passed as an argument to it
+*/
+
+
+/* 
+   Use charAt() function returns the character
+   at the given position in a string
+*/
+
+//print reverse each word string result
+
+}
